eaf35f208c438f6fd6cc8051c92dd17ec246d435
taha <tahachishti3@gmail.com>
2026-02-18 09:30:43 +0530

    Implemented UC1, UC2, UC3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("====================================");
+        System.out.println("   Welcome to Palindrome Checker   ");
+        System.out.println("   Version: 1.0                    ");
+        System.out.println("====================================");
+        System.out.println();
+
+
+        String hardcoded = "madam";
+        String hardcodedReversed = "";
+        for (int i = hardcoded.length() - 1; i >= 0; i--) {
+            hardcodedReversed = hardcodedReversed + hardcoded.charAt(i);
+        }
+        if (hardcoded.equals(hardcodedReversed)) {
+            System.out.println("[Hardcoded Check] \"" + hardcoded + "\" is a Palindrome.");
+        } else {
+            System.out.println("[Hardcoded Check] \"" + hardcoded + "\" is NOT a Palindrome.");
+        }
+        System.out.println();
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to check: ");
+        String original = scanner.nextLine();
+
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("\"" + original + "\" is a Palindrome.");
+        } else {
+            System.out.println("\"" + original + "\" is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
